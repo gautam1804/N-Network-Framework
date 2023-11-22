b92106d1715768929379ef9e185b9ab0edf3cbc7
Gautam Biloniya <gautambiloniya0418@gmail.com>
2023-11-22 23:08:29 +0530

    .

--- nframeworkserver/src/main/java/com/thinking/machines/network/server/Path.java
+++
@@ -1,9 +0,0 @@
-package com.thinking.machines.network.server;
-
-import java.lang.annotation.*;
-
-@Retention(RetentionPolicy.RUNTIME)
-@Target({ElementType.TYPE,ElementType.METHOD})
-public @interface Path {
-    public String value() default "";
-}
